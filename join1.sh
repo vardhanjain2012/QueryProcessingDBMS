4a7866632ff417d1e5a7ff57448744fb167ed6ab
Vardhan Jain <vardhanjain2012@gmail.com>
2021-04-22 20:07:21 +0530

    minor changes

--- join1.sh
+++ join1.sh
@@ -1,3 +1,4 @@
 make join1
+rm TestCases/TC_join1/myOutput
 ./join1 TestCases/TC_join1/input1_join1 TestCases/TC_join1/input2_join1 TestCases/TC_join1/myOutput
 diff TestCases/TC_join1/myOutput TestCases/TC_join1/output_join1
